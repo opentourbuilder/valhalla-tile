8c29176fd384655cde312fd6040460ce37dfb327
Roland Vu <51029874+Eyrie01@users.noreply.github.com>
2023-02-01 09:58:31 -0500

    Delete entrypoint.sh
    Did not need this

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-wget https://download.geofabrik.de/north-america/us/south-carolina-latest.osm.pbf
-mkdir -p valhalla_tiles
-valhalla_build_config > valhalla.json
-valhalla_build_tiles -c valhalla.json south-carolina-latest.osm.pbf
